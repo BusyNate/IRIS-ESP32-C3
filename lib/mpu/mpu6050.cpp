23adbfeba9319ca26b780d30fc27497a0d92c6c6
nathan-pulego <nathanm@pulego.co.za>
2025-09-01 20:51:30 +0200

    Refactored headers to be inside src folder

--- lib/mpu/mpu6050.cpp
+++ lib/mpu/mpu6050.cpp
@@ -15,9 +15,9 @@ bool MPU6050::testConnection() {
 
 void MPU6050::getAcceleration(int16_t* ax, int16_t* ay, int16_t* az) {
     Wire.beginTransmission(0x68);
-    Wire.write(0x3B);
+    Wire.write(0x3B);   
     Wire.endTransmission(false);
-    Wire.requestFrom(0x68, 6, true);
+    Wire.requestFrom(0x68, 6, true); //TODO: verify pin for request
     *ax = (Wire.read() << 8) | Wire.read();
     *ay = (Wire.read() << 8) | Wire.read();
     *az = (Wire.read() << 8) | Wire.read();
